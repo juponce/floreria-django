c4c71a2f56b18a90ceaaabd980c01082a5c91683
juponce <102298230+juponce@users.noreply.github.com>
2023-01-02 15:15:38 -0300

    Update settings.py

--- Examen_prog_web/settings.py
+++ Examen_prog_web/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'floreria',
         'USER': 'root',
-        'PASSWORD': '12345',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
